f32c50a51d14baba098e02195a8cd627b5914c41
Kirill Selyukov <kirill.selyukov@dfsco.com>
2018-03-15 15:36:59 +0200

    Set default global configuration for http communication

--- src/index.js
+++ src/index.js
@@ -6,19 +6,19 @@ import registerServiceWorker from './registerServiceWorker';
 
 import './index.css';
 
+axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com';
+axios.defaults.headers.common['Authorisation'] = 'AUTH KEY';
+axios.defaults.headers.post['Content-Type'] = 'application/json';
+
 axios.interceptors.request.use(request => {
-    console.log('request: ', request);
     return request;
 }, error => {
-    console.log('error: ', error);
     return Promise.reject(error);
 });
 
 axios.interceptors.response.use(response => {
-    console.log('response: ', response);
     return response;
 }, error => {
-    console.log('error: ', error);
     return Promise.reject(error);
 });
 
