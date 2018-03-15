06c4121ac55e83504f916dcd80dbedae98b6ad62
Kirill Selyukov <kirill.selyukov@dfsco.com>
2018-03-15 14:40:37 +0200

    Add ability to Delete post

--- src/components/NewPost/NewPost.js
+++ src/components/NewPost/NewPost.js
@@ -8,7 +8,7 @@ class NewPost extends Component {
         title: '',
         content: '',
         author: 'Max'
-    }
+    };
 
     postDataHandler = () => {
         const post = {
@@ -20,8 +20,9 @@ class NewPost extends Component {
         axios.post('https://jsonplaceholder.typicode.com/posts', post)
             .then((response) => {
                 console.log(response);
-            })
+            });
     }
+
     componentWillUpdate() {
         console.log('[NewPost]componentWillUpdate');
     }
